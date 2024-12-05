87afc6671ae7713fbcea7c166e8f9c8f59f83e05
Трагель Вячеслав Викторович <v.tragel@cmachine.ru>
2024-12-06 08:56:56 +1000

    mapping

--- server/api/helper.php
+++ server/api/helper.php
@@ -2,6 +2,16 @@
 
 class helper
 {
+    static function getCurrentUser(){
+        $token = getAuthToken();
+        if($token){
+            $user = userModel::with("role")->where('hash',$token)->first();
+            if($user){
+                return $user->toArray();
+            }
+        }
+        return null;
+    }
     static function checkPermission($permissionName,$params,$withView=true){
         if(isset($params->$permissionName)){
             if($params->$permissionName!=="no"){
--- server/api/services/users.php
+++ server/api/services/users.php
@@ -2,34 +2,36 @@
 _load("**.helper");
 class UsersService  extends CService
 {
+    private $user;
     public function __construct() {
         parent::__construct();
+        $this->user = helper::getCurrentUser();
     }
     public function getAll(){
         $res = new jsonClass([]);
         $result =  UserModel::with("role")->get()->toArray();
         if($result!=null){
             foreach ($result as $user) {
-                $item = $this->mapper->map($user,"UserDto",["role"=>function($x){return $this->mapper->map($x->role,"RoleDto");}]);
+                $item = $this->userDtoMapping($user);
                 $res->result[] = $item;
             }
             
         }
         return $res;
     }
-    /**
-     * получение всех необходимых данных для пользователя сайта
-     */
     public function getMainData(){
 
         $res = new jsonClass();
         $res->result = (object)array(
-            "token"=>getAuthToken(),
-            "user"=>null,
+            "user"=>$this->user?$this->userDtoMapping($this->user):null,
             "appVersion"=>_AppVersion
         );
        
         return $res;
+    }
+
+    private function userDtoMapping($user){
+        return $this->mapper->map($user,"UserDto",["role"=>function($x){return $this->mapper->map($x->role,"RoleDto");}]);
     }
 }
 ?>
